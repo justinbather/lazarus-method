e60671f65769acc5f9503cc14942038e672f532f
justinbather <justinbather@hotmail.com>
2023-05-23 14:25:43 -0400

    change static url

--- lazarusapp/settings.py
+++ lazarusapp/settings.py
@@ -231,7 +231,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'user/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
